b5c891d5671ed676ac052edf1dacf189c8e0c7de
gcutrell <gcutrell@limno.com>
2020-04-23 15:03:41 -0400

    Removed ubidots check
    Removed checking for most recent variable time from ubidots. Now save local file with last variable time.

--- ubidots_upload.py
+++ ubidots_upload.py
@@ -14,71 +14,59 @@ global variables
 BASE_DIR=os.path.dirname(os.path.realpath(__file__))
 #FolderName = ''			#Use if placed in folder above data.  
 ENDPOINT = "industrial.api.ubidots.com"
-device = "SanduskyBay_NuLab"
+device = "PotatoRun_NuLab"
 token = "BBFF-3SW5GCgLEF4DVUpYRNMylqFyXAFUDA"	#LTI Token
 buoyDateFormat = '%m/%d/%Y %H:%M:%S'
 
-
-def get_lastSample(url, device, variable,token):
-	try:
-		url = "https://{}/api/v1.6/devices/{}/{}/lv".format(url,device,variable)
-		headers = {"X-Auth-Token": token, "Content-Type": "application/json"}
-		attempts = 0
-		status_code = 400
-		while status_code >= 400 and attempts < 5:
-			print("[INFO] Retrieving data, attempt number: {}".format(attempts))
-			req = requests.get(url=url, headers=headers)
-			status_code = req.status_code
-			attempts += 1
-			time.sleep(1)
-
-		#print("[INFO] Results:")
-		print("Last {} retrieved for {} is {}".format(variable,device,float(req.text)))
-		return(float(req.text))
-	except Exception as e:
-		print("[ERROR] Error posting, details: {}".format(e))
-
-def get_valueExistence(url, variable, dateTime):	
-	try:
-		url = "https://{}/api/v1.6/devices/{}/{}/values?page=1?page_size=1?start={}?end={}?format=json".format(url,device,variable,dateTime,dateTime)
-		headers = {"X-Auth-Token": token, "Content-Type": "application/json"}
-		attempts = 0
-		status_code = 400
-		while status_code >= 400 and attempts < 5:
-			#print("[INFO] Retrieving data, attempt number: {}".format(attempts))
-			req = requests.get(url=url, headers=headers)
-			status_code = req.status_code
-			attempts += 1
-			time.sleep(1)
+def get_valueSentCheck(variable,dateTime):
 		
-		#print("[INFO] Results:")
-		jsonResponse = json.loads(req.text)
-		if (dateTime == jsonResponse["results"][0]["timestamp"]):
-			print("Station {} value {} is already up-to-date.".format(device,variable))
-			return True
+	dctData = {}
+	#If file does not exist then create file and add param and time.
+	if os.path.isfile("ubidots_lastSend.json"):
+		f = open("ubidots_lastSend.json","r")
+		#If error then file exists but is empty. Create variable key and assign 0 time. 
+		try:		
+			dctData = json.load(f)
+			
+		except Exception:
+			dctData = {}
+			dctData[variable] = 0
+		f.close()
+		#Check if param exists. No add it to dict. Yes extract time and check if new.
+		if variable in dctData:
+			lastValueSentTime = dctData[variable]
+			if (dateTime == lastValueSentTime):
+				print("Station {} value {} has already been sent.".format(device,variable))
+				sent = True
+			else:
+				print("Station {} value {} has not been sent. Adding to payload".format(device,variable))
+				dctData[variable] = dateTime
+				sent = False
 		else:
-			print("Station {} value {} is not up-to-date. Adding to payload".format(device,variable))
-			return False
+			dctData[variable] = dateTime
+			print("Station {} value {} has not been sent. Adding to payload".format(device,variable))		
+			sent = False
+	else:
+		dctData = {variable:dateTime}
+		sent = false
 		
-		return(float(req.text))
-	except Exception as e:
-		print("[ERROR] Error posting, details: {}. No values may be avaiable for variable {}. Try to add value".format(e,variable))
-		return False
+	#Write out dict whether or not modified		
+	f2 = open('ubidots_lastSend.json','w+')	
+	f2.write(json.dumps(dctData, indent=1))
+	f2.close()
+	return sent
 		
 def getKeyPayload(filePath,channel):
 	
 	payload = {}
-
 	#Build Array of stations to post to ubidots
 	lines = open(filePath).readlines()
 	
 	#Extract header to be used as key and latestDataRow as value for httppost.
 	variables = lines[0].strip().split(",")
-
 	#Remove any paranethesis from variable names to remove changes in units from nulabs
 	for i in range(len(variables)):
 		variables[i] = variables[i].split('(')[0]
-
 	#If macro header is not listed, then file contains no needed data and skip.
 	try:
 		macroLoc = variables.index("Macro")
@@ -98,7 +86,6 @@ def getKeyPayload(filePath,channel):
 		except:
 			TP_SmpconcLoc = variables.index("TP_Smp_Conc")
 			TP_SmpabsLoc = variables.index("TP_Smp_Abs")
-	
 	macro1 = []
 	macro2 = []
 	macro8 = []
@@ -110,17 +97,15 @@ def getKeyPayload(filePath,channel):
 			if data[macroLoc] == "M1" and data[SmpconcLoc]:
 				macro1.append(data)
 			elif data[macroLoc] == "M2" and data[OBSconcLoc]:
-				macro2.append(data)	
+				macro2.append(data)
 			elif data[macroLoc] == "M8" and data[15]:
 				macro8.append(data)
-				
+					
 		except:
 			pass
-
 	#Check for empty data for macro1 or macro2 results. If empty then return empty payload
-	if not macro1 or not macro2:
+	if not (macro1 or macro2 or macro8):
 		return payload
-
 	#Create Unix time in milliseconds from first timestamp always located in first array location
 	SmpTimestamp = []
 	ObsTimestamp = []
@@ -132,13 +117,12 @@ def getKeyPayload(filePath,channel):
 	except:
 		SmpDateTime = datetime.strptime(macro8[0][0],buoyDateFormat)	
 		SmpTimestamp = ((SmpDateTime - datetime(1970,1,1)).total_seconds())*1000
-
 	for x in range(len(variables)):
 		if variables[x] in ['OBS_abs','OBS_conc']:
-			dateTime = macro2Timestamp
+			dateTime = ObsTimestamp
 			dataSend = macro2
 		elif variables[x] in ['Smp_abs','Smp_conc']:
-			dateTime = macro1Timestamp
+			dateTime = SmpTimestamp
 			dataSend = macro1
 		elif variables[x] in ['TP_Smp_Abs','TN_Smp_Abs','TP_Smp_Conc','TN_Smp_Conc']:
 			#Remove TN_/TP_ from variable name
@@ -149,7 +133,7 @@ def getKeyPayload(filePath,channel):
 		elif variables[x] in ['Light']:
 			paramName1 = channel+"-Smp_"+variables[x]
 			paramName2 = channel+"-OBS_"+variables[x]
-			updateValue1 = get_valueExistence("industrial.api.ubidots.com", paramName1, macro1Timestamp)
+			updateValue1 = get_valueSentCheck(paramName1, SmpTimestamp)
 			if (updateValue1 is False):
 				#NaN's will cause error. Pass sending variable. 
 				try:
@@ -163,7 +147,7 @@ def getKeyPayload(filePath,channel):
 						continue
 			#Only check if there is an OBS 			
 			if (ObsTimestamp):
-				updateValue2 = get_valueExistence("industrial.api.ubidots.com", paramName2, ObsTimestamp)
+				updateValue2 = get_valueSentCheck(paramName2, ObsTimestamp)
 				if (updateValue2 is False):
 					#NaN's will cause error. Pass sending variable. 
 					try:
@@ -177,9 +161,9 @@ def getKeyPayload(filePath,channel):
 				continue
 		else:
 			continue
-			
 		paramName = channel+"-"+variables[x]
-		updateValue = get_valueExistence("industrial.api.ubidots.com", paramName, dateTime)
+		
+		updateValue = get_valueSentCheck(paramName, dateTime)
 		if (updateValue is False):
 			#NaN's will cause error. Pass sending variable. 
 			try:
@@ -190,6 +174,7 @@ def getKeyPayload(filePath,channel):
 	return payload
 	
 def post_var(payload, url, device, token):
+
 	try:
 		url = "https://{}/api/v1.6/devices/{}".format(url, device)
 		headers = {"X-Auth-Token": token, "Content-Type": "application/json"}
@@ -202,36 +187,32 @@ def post_var(payload, url, device, token):
 			status_code = req.status_code
 			attempts += 1
 			time.sleep(1)
-
 		#print("[INFO] Results:")
 		print(req.text)
 		print("Data has been succesfully sent to Ubidots for station {}.".format((device)))
 	except Exception as e:
 		print("[ERROR] Error posting for station {}, details: {}".format(device,e))
-
 	
 def main():
     
 	print("Runtime at {}.".format(datetime.now()))
 	#Get sensor values from each defined buoy file and send to ubidots
-
 	#Retrieve all files that end in .txt. Extract channel type from basename.	
 	#dataList = glob.glob(os.path.join(BASE_DIR,FolderName+'/'+'*-L.txt'))	
 	dataList = glob.glob(os.path.join(BASE_DIR,'*-L.txt'))	
-
 	for x in dataList:
 		base = os.path.basename(x)
 		channel = os.path.splitext(base)[0]
 		channel = channel.split('_')[0]
 		if channel == "N+N":
 			channel = "N-N"
-
+		print(channel)
 		#Retrieve data from source and build payload from file
 		payload = getKeyPayload(x,channel)
-
 		# Send try sending to Ubidots if there is data in payload
 		if payload:
-			post_var(payload,"industrial.api.ubidots.com",device,token)
+			#post_var(payload,"industrial.api.ubidots.com",device,token)
+			print(payload)
 		else:
 			print("No data for channel {}.".format(channel))
 	print("Run finished at {}.".format(datetime.now()))
